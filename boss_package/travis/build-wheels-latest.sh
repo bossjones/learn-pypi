ac156ebbec2b917713f602496917272f256f7f26
Malcolm Jones <bossjones@theblacktonystark.com>
2019-06-10 23:12:12 -0400

    wip: wheelhouse changes

--- boss_package/travis/build-wheels-latest.sh
+++ boss_package/travis/build-wheels-latest.sh
@@ -5,10 +5,11 @@ set -e -x
 
 # Install a system package required by our library
 yum install -y atlas-devel
+yum install cairo-devel pycairo -y
 
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+for PYBIN in /opt/python/cp3{6,7}*/bin; do
+    "${PYBIN}/pip" install -r /io/requirements-dev.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
@@ -18,7 +19,7 @@ for whl in wheelhouse/*.whl; do
 done
 
 # Install packages and test
-for PYBIN in /opt/python/*/bin/; do
+for PYBIN in /opt/python/cp3{6,7}*/bin/; do
     "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
     (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
 done
